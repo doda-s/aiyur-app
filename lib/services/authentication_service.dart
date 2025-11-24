c072d2408997f1eb234ca285c401688b6fa80501
doda-s <ph005009004@gmail.com>
2025-11-24 19:47:26 -0300

    add get user credential method to authentication service

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -1,4 +1,5 @@
 import 'package:aiyurapp/modules/authentication_module.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<AuthResult> createUserWithEmailAndPassword(String emailAddress, password) async {
   return await AuthenticationModule.createUserWithEmailAndPassword(emailAddress, password);
@@ -10,4 +11,15 @@ Future<AuthResult> signInUserWithEmailAndPassword(String emailAddress, password)
 
 Future<void> signOutUser() async {
   await AuthenticationModule.signOut();
+}
+
+UserCredential getUserCredential() {
+  UserCredential? userCredential = AuthenticationModule.getUserCredential();
+
+  if (userCredential != null) {
+    return userCredential;
+  }
+
+  // TODO force user to authenticate
+  throw Exception("User autentication not implemented");
 }
